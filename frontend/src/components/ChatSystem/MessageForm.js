ecbc07d4b4cae17269adb34c33cf0c1b40979496
Maceina <maceinaaudrius@gmail.com>
2020-11-16 14:05:38 +0200

    Message and email form done

--- frontend/src/components/ChatSystem/MessageForm.js
+++ frontend/src/components/ChatSystem/MessageForm.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import "./MessageForm.css";
+import Button from 'react-bootstrap/Button'
+import Form from "react-bootstrap/Form";
 
 function MessageForm() {
     return (
         <div>
-            <h2>Message form</h2>
+            <h2 className="messageHeader">Pradėkite pokalbį</h2>
+            <div className="form">
+      <Form>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Žinutė:</Form.Label>
+          <Form.Control as="textarea" rows={4} />
+        </Form.Group>
+      </Form>
+      <div className="sendButton">
+      <Button variant="primary">Siųsti</Button>
+      </div>
+      </div>
         </div>
     )
 }
